a1ad38b1eea52170ecf62f079a12f67e74b65163
danielvargass97 <daniel_vrgs@hotmail.com>
2023-04-11 09:25:17 -0500

    Create service "RickAndMorty" and impl

---
+++ src/main/java/com/farmatodo/technical/bussines/service/impl/RickAndMortyServiceImpl.java
@@ -0,0 +1,10 @@
+package com.farmatodo.technical.bussines.service.impl;
+
+import com.farmatodo.technical.bussines.service.RickAndMortyService;
+
+public class RickAndMortyServiceImpl implements RickAndMortyService {
+    @Override
+    public void getEpisodeDescriptionByEpisodeId(String episodeId) {
+        //TODO
+    }
+}
